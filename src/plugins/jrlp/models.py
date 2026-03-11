3983aa760f69b65be91f5f439c2d47712ae01658
Tano <github@tano.asia>
2026-03-12 00:52:32 +0800

    fix: 修改数据库模型

--- src/plugins/jrlp/models.py
+++ src/plugins/jrlp/models.py
@@ -4,8 +4,8 @@ from sqlalchemy import Column, String, INTEGER
 
 class JrlpContent(Model):
     __tablename__ = "JrlpContent"
-    id = Column(String(255), primary_key=True, nullable=True)
-    user_id = Column(INTEGER, nullable=True)
-    group_id = Column(INTEGER, nullable=True)
+
+    user_id = Column(INTEGER, nullable=True, primary_key=True)
+    group_id = Column(INTEGER, nullable=True, primary_key=True)
     wife_id = Column(INTEGER, nullable=True)
     match_date = Column(String(20), nullable=False)  # 存储日期字符串 YYYY-MM-DD
